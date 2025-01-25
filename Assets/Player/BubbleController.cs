0bf1d05174bc01a1067dbf74c70941f5288b3e3e
Robert Linsalato <robertlinsalato21@apu.edu>
2025-01-25 01:22:20 -0800

    Bubble fix and Game Session
    - Bubble now lerps between states
    - Added GameSession, allows scenes to switch and player can quit the game easier

--- Assets/Player/BubbleController.cs
+++ Assets/Player/BubbleController.cs
@@ -1,26 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Data.Common;
 using UnityEngine;
 
 public class BubbleController : MonoBehaviour
 {
     SphereCollider col;
     public int socialLevel = 10;
+    private Vector3 targetScale;
+    [SerializeField] [Range (1, 10)] float LerpTimeMultiplier = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
         col = GetComponent<SphereCollider>();
+        targetScale = new Vector3(socialLevel, socialLevel, socialLevel);  // Set initial target scale
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.localScale = new Vector3(socialLevel, socialLevel, socialLevel);
+        // Smoothly interpolate between current scale and target scale
+        transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.deltaTime * LerpTimeMultiplier);
     }
 
     public void EntityEnteredBubble(int socLevDecreese){
         socialLevel = socialLevel - socLevDecreese;
+        targetScale = new Vector3(socialLevel, socialLevel, socialLevel);  // Update target scale
     }
 }
