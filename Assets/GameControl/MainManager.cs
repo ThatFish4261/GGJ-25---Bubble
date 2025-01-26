ede2db36a6a2c3f49ae1659e16c9e48484669833
Robert Linsalato <linsalato.2022@gmail.com>
2025-01-26 14:48:38 -0800

    FINAL COMMIT YIPPIE
    - I forgot to keep track of what i did, whoops (it was the last bit, idk man)
    - Added sick background to the menus
    - Pressing [ESC] quits the whole game :)
    - Added Main menu button

--- Assets/GameControl/MainManager.cs
+++ Assets/GameControl/MainManager.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
 {
     public static MainManager Instance;
+
+    [SerializeField] public Canvas nonMenuCanvas;
+
+    public Text text;
+    [SerializeField] public Image textHolder;
 
     public int livesRemaining = 5;
 
@@ -19,13 +26,74 @@ public class MainManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+
+        //------------------------------------//
+
+        string activeScene = SceneManager.GetActiveScene().name;
+        print(activeScene);
+
+        text.enabled = false;
+
+        nonMenuCanvas.enabled = true;
+
+        if(activeScene == "Level1 - Train"){
+            text.text = "There’s always something eery about coming home\nEverytime I have to pass someon\nIt gives me jitters from skin to bone\nI can’t even remember a time not like this\nOne where I haven’t wanted to be alone";
+            text.enabled = true;
+            textHolder.enabled = true;
+            Cursor.visible = false;
+            StartCoroutine(HideIntroText());
+            
+        }
+
+        else if(activeScene == "Level2 - Mall"){
+            text.text = "Even as a teenager, I couldn’t stand crowds\nFrom the kids running amuck to the obnoxious screamers,\nI’d want to put up my own shroud";
+            text.enabled = true;
+            textHolder.enabled = true;
+            StartCoroutine(HideIntroText());
+        }
+
+
+        else if(activeScene == "Level3 - IceRink"){
+            text.enabled = false;
+            textHolder.enabled = false;
+        }
+
+        else if(activeScene == "Game Over" || activeScene == "Game Win" || activeScene == "Main Menu"){
+            nonMenuCanvas.enabled = false;
+            Cursor.visible = true;
+        }
+
+        else{
+            text.enabled = false;
+            textHolder.enabled = false;
+        }
     }
 
+    private void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            #if UNITY_STANDALONE
+                    Application.Quit();
+            #endif
+
+            #if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+        }
+    }
+    
     public void LoseLife(){
         livesRemaining--;
     }
 
     public void GameOver(){
         SceneManager.LoadScene("GameOver");
+        Cursor.visible = true;
+    }
+
+    public IEnumerator HideIntroText(){
+        yield return new WaitForSecondsRealtime(7.5f);
+
+        textHolder.enabled = false;
+        text.enabled = false;
     }
 }
--- Assets/GameControl/MenuControls.cs
+++ Assets/GameControl/MenuControls.cs
@@ -6,22 +6,42 @@ using UnityEngine.SceneManagement;
 public class MenuControls : MonoBehaviour
 {
     MainManager mainManager;
-    
+    public GameObject aboutParent;
+    public GameObject homeParent;
+
     // Start is called before the first frame update
     void Start()
     {
         mainManager = FindAnyObjectByType<MainManager>();
+        aboutParent.SetActive(false);
+        homeParent.SetActive(true);
+
+        aboutParent.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update(){
+        Cursor.visible = true;
     }
 
     public void StartGame(){
         SceneManager.LoadScene("Level1 - Train");
-        mainManager.livesRemaining = 5;
+        mainManager.livesRemaining = 1;
+        Cursor.visible = false;
+    }
+
+    public void MainMenu(){
+        SceneManager.LoadScene("MainMenu");
+        Cursor.visible = true;
+    }
+
+    public void ToAbout(){
+        aboutParent.SetActive(true);
+        homeParent.SetActive(false);
+    }
+
+    public void Back(){
+        aboutParent.SetActive(false);
+        homeParent.SetActive(true);
     }
 
     public void QuitGame(){
