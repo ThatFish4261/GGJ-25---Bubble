8088a5530941c6dedfcd2394d52904bb245bef41
Robert Linsalato <linsalato.2022@gmail.com>
2025-01-25 18:35:00 -0800

    Added child
    - Made children rotate based off direction of travel
    - Added child models, animation, updated code
    - added fence to lv 3
    - random bug fixes

--- Assets/AI Stuff/PathFollower.cs
+++ Assets/AI Stuff/PathFollower.cs
@@ -9,21 +9,38 @@ public class PathFollower : MonoBehaviour
     public int currentPoint = 0;
     private bool isWaiting = false;
 
+    [SerializeField] Animator animator;
+
     void Start()
     {
+        animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
         if (!isWaiting)
         {
+            animator.speed = 1f;
+
             float dist = Vector3.Distance(path[currentPoint].position, transform.position);
             transform.position = Vector3.MoveTowards(transform.position, path[currentPoint].position, Time.deltaTime * speed);
+
+            // Rotate based on velocity (direction of movement)
+            Vector3 direction = (path[currentPoint].position - transform.position).normalized;
+            if (direction != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * speed);
+            }
 
             if (dist <= reachDist)
             {
                 StartCoroutine(WaitAtPoint());
             }
+        }
+        else
+        {
+            animator.speed = 0f;
         }
     }
 
